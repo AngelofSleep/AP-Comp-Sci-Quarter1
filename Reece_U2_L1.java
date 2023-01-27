d3df3fa4d71a2a4c827461dc43ba284e584ffdd2
Kathleen Reece <123769977+AngelofSleep@users.noreply.github.com>
2023-01-27 14:38:28 -0500

    Update Reece_U2_L1.java

--- Reece_U2_L1.java
+++ Reece_U2_L1.java
@@ -2,6 +2,7 @@ public class Reece_U2_L1
 {
    public static void main(String[] args)
    {
+      //this lab requires Pokemon.java, which your teacher will give to you
       Pokemon a = new Pokemon();
       Pokemon b = new Pokemon("Charmander", "Fire", 10, 90);
       a.displayStats();
