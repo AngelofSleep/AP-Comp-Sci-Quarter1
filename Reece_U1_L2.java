4c54c557fd72d1a20c82ec75a89da47c8d05b305
AngelofSleep <123769977+AngelofSleep@users.noreply.github.com>
2023-01-27 14:30:30 -0500

    Update Reece_U1_L2.java

--- Reece_U1_L2.java
+++ Reece_U1_L2.java
@@ -4,21 +4,22 @@ public class Reece_U1_L2
    public static void main(String[] args)
    {
       Scanner mae = new Scanner(System.in);
-      double rad,hei;
+      double rad,hei;//rad = radius of cone, hei=height of the cone
       System.out.print("Enter the radius of the cone: ");
       rad = mae.nextDouble();
       System.out.print("Enter the height of the cone: ");
       hei = mae.nextDouble();
-      double pi = Math.PI;
+      double pi = Math.PI; //pi = value of pi. You could also just type Math.PI evertime you need to use pi
       double srad = Math.pow(rad,2);
       double shei = Math.pow(hei,2);
-      double dia = rad * 2;
-      double cir = (rad * 2) * pi;
+      double dia = rad * 2; //diameter
+      double cir = (rad * 2) * pi;//circumferance
       double preroot = srad + shei;
       double root = Math.sqrt(preroot);
-      double are = (rad + root) * pi * rad;
+      double are = (rad + root) * pi * rad; //surface area
       double third = 1.0/3.0;
-      double vol = third * pi * srad * hei;
+      double vol = third * pi * srad * hei; //volume
       System.out.print("The diameter is " + dia + ", the circumference is " + cir + ", the surface area is " + are + ", and the volume is " + vol + ".");
+      //looking back, this is way too many doubles
    }
 }
